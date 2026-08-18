d6481e5839aead0a2029e1619d026bb87759e121
juankurtzz <juanpietro1@outlook.com>
2026-08-17 21:51:45 -0300

    refactor all api

--- prisma/migrations/20260206003641_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Podcast" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "podcastNAME" TEXT NOT NULL,
-    "episode" TEXT NOT NULL,
-    "videoID" TEXT NOT NULL,
-    "categories" TEXT NOT NULL
-);
